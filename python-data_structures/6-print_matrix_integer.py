df738cd75914c4ea69829d9321f580ca610e0dd3
Gabriel <gabyrivm06@gmail.com>
2024-09-09 14:39:42 -0400

    Task 6 second test

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -3,6 +3,6 @@
 def print_matrix_integer(matrix=[[]]):
     for row in matrix:
         for element in row:
-            print(" ".join("{:d}".format(element)), end="")
+            print(" ".join("{:d} ".format(element)), end="")
 
         print()
