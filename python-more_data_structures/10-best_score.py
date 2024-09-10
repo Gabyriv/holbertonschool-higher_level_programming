893dd43f26660cbd0ef122a9cc491a13f0992d63
Gabriel <gabyrivm06@gmail.com>
2024-09-10 14:30:04 -0400

    pycodestyle fix

--- python-more_data_structures/10-best_score.py
+++ python-more_data_structures/10-best_score.py
@@ -4,9 +4,9 @@ def best_score(a_dictionary):
     if a_dictionary is None or not a_dictionary:
         return None
 
-    # Initialize variables to keep track of the best score and corresponding key
     best_key = None
-    best_value = -float('inf')  # this is to start with the smallest possible value
+    # this is to start with the smallest possible value
+    best_value = -float('inf')  
 
     for key, value in a_dictionary.items():
         if value > best_value:
