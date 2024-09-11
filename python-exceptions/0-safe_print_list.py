fce02d2004d2bbde6b4447e989698de6e01a7036
Gabriel <gabyrivm06@gmail.com>
2024-09-11 10:36:25 -0400

    pycodestyle fix

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 
 def safe_print_list(my_list=[], x=0):
-        printed_elements = 0
-        try:
-            for i in range(x):
-                print(my_list[i], end="")
-                printed_elements += 1
+    printed_elements = 0
+    try:
+        for i in range(x):
+            print(my_list[i], end="")
+            printed_elements += 1
 
-        except IndexError:
-            pass
+    except IndexError:
+        pass
 
-        print()
-        return printed_elements
+    print()
+    return printed_elements
