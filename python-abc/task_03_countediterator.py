e3fe9c610ab14673b09688c52ebd7c1253e2c559
Gabriel <gabyrivm06@gmail.com>
2024-09-26 10:37:47 -0400

    CountedIterator - Keeping Track of Iteration

--- python-abc/task_03_countediterator.py
+++ python-abc/task_03_countediterator.py
@@ -2,53 +2,71 @@
 """Define a class named CountedIterator."""
 
 
-class CountedIterator():
-    """CountedIterator"""
+class CountedIterator:
+    """
+    An iterator class that keeps track of how many items have been iterated.
 
-    def __init__(self, iterator):
-        """Constructor.
+    Attributes:
+        iterator (iter): The original iterator object.
+        counter (int): Tracks the number of items iterated.
+    """
+
+    def __init__(self, iterable):
+        """
+        Initialize the CountedIterator with an iterable.
 
         Args:
-            iterator (iterable): Iterates the list to update the counter.
-            counter (int): The counter tracks the number of items iterated.
+            iterable (iterable): An iterable object that
+                                    will be converted to an iterator.
         """
-        self.iterator = iter(iterator)
+        self.iterator = iter(iterable)
         self.counter = 0
 
     def get_count(self):
-        """Returns the current value of the counter"""
+        """
+        Return the current value of the counter.
+
+        Returns:
+            int: Number of items iterated over.
+        """
         return self.counter
 
     def __iter__(self):
-        """Returns:
-            The iterator
+        """
+        Return the iterator object itself.
+
+        Returns:
+            CountedIterator: The current instance.
         """
         return self
 
     def __next__(self):
-        """Modified the __next__ method to get the next item
-            from the iterator and increments the counter.
+        """
+        Fetch the next item from the iterator and increment the counter.
 
         Raises:
-            StopIteration: if there is no more items
+            StopIteration: If there are no more items in the iterator.
 
         Returns:
-            The next item from the list.
+            The next item from the iterator.
         """
-        self.counter += 1
         try:
-            return next(self.iterator)
+            item = next(self.iterator)
+            self.counter += 1
+            return item
         except StopIteration:
-            raise StopIteration
+            raise
 
 
 # Testing
-data = [1, 2, 3, 4]
-counted_iter = CountedIterator(data)
+if __name__ == "__main__":
+    data = [1, 2, 3, 4]
+    counted_iter = CountedIterator(data)
 
-try:
-    while True:
-        item = next(counted_iter)
-        print(f"Got {item}, total {counted_iter.get_count()} items iterated.")
-except StopIteration:
-    print("No more items.")
+    try:
+        while True:
+            item = next(counted_iter)
+            print(f"Got {item}, total"
+                  f"{counted_iter.get_count()} items iterated.")
+    except StopIteration:
+        print("No more items.")
