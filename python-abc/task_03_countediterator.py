137790a9fce27414ae02a7ca719af302e2896d66
Gabriel <gabyrivm06@gmail.com>
2024-09-26 09:44:49 -0400

    CountedIterator - Keeping Track of Iteration

---
+++ python-abc/task_03_countediterator.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""Define a class named CountedIterator."""
+
+
+class CountedIterator:
+    """CountedIterator"""
+
+    def __init__(self, iterator):
+        """Constructor.
+
+        Args:
+            iterator (iterable): Iterates the list to update the counter.
+            counter (int): The counter tracks the number of items iterated.
+        """
+        self.iterator = iter(iterator)
+        self.counter = 0
+
+    def get_count(self):
+        """Returns the current value of the counter"""
+        return self.counter
+
+    def __iter__(self):
+        """Returns:
+            The iterator
+        """
+        return self
+
+    def __next__(self):
+        """Modified the __next__ method to get the next item
+            from the iterator and increments the counter.
+
+        Raises:
+            StopIteration: if there is no more items
+
+        Returns:
+            The next item from the list.
+        """
+        self.counter += 1
+
+        try:
+            return next(self.iterator)
+        except:
+            raise StopIteration
