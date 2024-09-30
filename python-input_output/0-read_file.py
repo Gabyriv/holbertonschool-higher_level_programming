58e1142245e970b080af1fa92c17c52765d06864
Gabriel <gabyrivm06@gmail.com>
2024-09-30 09:47:22 -0400

    change variable name in the line 12

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -9,5 +9,5 @@ def read_file(filename=""):
     Args:
         filename (str): name of file to read. Defaults to "".
     """
-    with open('my_file_0.txt', encoding="utf-8") as f:
+    with open(filename, encoding="utf-8") as f:
         print(f.read(), end="")
