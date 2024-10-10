c8b7b237a2c110e5ddaa773b09ed2f9073395330
Gabriel <gabyrivm06@gmail.com>
2024-10-10 13:08:33 -0400

    Develop a Simple API using Python with Flask

---
+++ restful-api/task_04_flask.py
@@ -0,0 +1,73 @@
+#!/usr/bin/python3
+"""Simple API using Python with Flask"""
+
+
+from flask import Flask
+from flask import jsonify
+from flask import request
+
+app = Flask(__name__)
+
+users = {
+    "jane": {
+        "username": "jane",
+        "name": "Jane",
+        "age": 28,
+        "city": "Los Angeles"
+    },
+    "john": {
+        "username": "john",
+        "name": "John",
+        "age": 30,
+        "city": "New York"
+    }}
+
+
+@app.route("/")
+def home():
+    return "Welcome to the Flask API!"
+
+
+@app.route("/data")
+def json_data():
+    usernames = list(users.keys())
+    return jsonify(usernames)
+
+
+@app.route("/status")
+def status():
+    return ("OK")
+
+
+# Dynamic route feature in flask
+@app.route("/users/<username>")
+def get_users(username):
+    user = users.get(username)
+    if user:
+        return jsonify(user)
+    else:
+        return jsonify({"error": "User not found"}), 404
+
+
+@app.route("/add_user", methods=["POST"])
+def add_user():
+    data = request.get_json()
+
+    username = data.get('username')
+    name = data.get('name')
+    age = data.get('age')
+    city = data.get('city')
+
+    if not all([username, name, age, city]):
+        return jsonify({"error": "Missing data"}), 400
+
+    if username in users:
+        return jsonify({"error": "Username already exists"}), 400
+
+    users[username] = {"name": name, "age": age, "city": city}
+
+    return jsonify({"message": "User added successfully", "user": users[username]}), 201
+
+
+if __name__ == "__main__":
+    app.run()
