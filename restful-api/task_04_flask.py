816876fe9da3bd102df743d18acb1d015d2d2e1f
Gabriel <gabyrivm06@gmail.com>
2024-10-11 14:27:04 -0400

    changed the status code for the duplicate username error from 400 to 409 conflict

--- restful-api/task_04_flask.py
+++ restful-api/task_04_flask.py
@@ -43,7 +43,7 @@ def add_user():
 
     username = data['username']
     if username in users:
-        return jsonify({"error": "Username already exists"}), 400
+        return jsonify({"error": "Username already exists"}), 409
 
     new_user = {
         "username": username,
