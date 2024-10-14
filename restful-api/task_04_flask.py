619c43a4b5f468d82125fb97d8b04f5f65d73aad
Gabriel <gabyrivm06@gmail.com>
2024-10-14 09:11:07 -0400

    Duplicated username

--- restful-api/task_04_flask.py
+++ restful-api/task_04_flask.py
@@ -37,25 +37,12 @@ def get_user(username):
 
 @app.route('/add_user', methods=['POST'])
 def add_user():
-    data = request.json
-    if not data or 'username' not in data:
-        return jsonify({"error": "Username is required"}), 400
-
-    username = data['username']
-    if username in users:
-        return jsonify({"error": "Username already exists"}), 409
-
-    new_user = {
-        "username": username,
-        "name": data.get('name'),
-        "age": data.get('age'),
-        "city": data.get('city')
-    }
-    users[username] = new_user
-    return jsonify({
-        "message": "User added",
-        "user": new_user
-    }), 201
+    data = request.get_json()
+    username = data.get("username")
+    if not username:
+        return jsonify({"error": "username is required"}), 400
+    users[username] = data
+    return jsonify({"message": "User added", "user": data}), 201
 
 
 if __name__ == "__main__":
