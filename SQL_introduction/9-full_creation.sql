6fbda546589d7d5a31f387431b1505b7a8f70a45
Gabriel Rivera <gabyrivm06@gmail.com>
2024-10-21 19:45:17 +0000

    Add comment for the file

--- SQL_introduction/9-full_creation.sql
+++ SQL_introduction/9-full_creation.sql
@@ -1,3 +1,4 @@
+-- Script that creates a table "second_table" in the database in your MySQL server and add multiples rows.
 CREATE TABLE IF NOT EXISTS second_table (
     id INT,
     name VARCHAR(256),
