5a4794fe2a5bd4c29587313f465f6c698ec5da16
Gabriel <gabyrivm06@gmail.com>
2024-10-28 14:07:49 -0400

    Test

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -4,9 +4,10 @@ import sys
 
 if __name__ == "__main__":
     db = MySQLdb.connect(
-        host="localhost",
+        host='localhost',
         port=3306,
         user=sys.argv[1],
+        password=sys.argv[2],
         db=sys.argv[3]
     )
 
