9da92626e0f6bb35187a300a0782d65827becb8f
Gabriel <gabyrivm06@gmail.com>
2024-10-29 18:27:12 -0400

    tst

--- python-object_relational_mapping/1-filter_states.py
+++ python-object_relational_mapping/1-filter_states.py
@@ -1,34 +1,28 @@
 #!/usr/bin/python3
 
 """
-Script that lists all states with a name
-starting with the letter 'N' from the database.
+Filter states by name.
 """
 
-import sys
 import MySQLdb
+import sys
 
-if __name__ == '__main__':
-
-    username = sys.argv[1]
-    password = sys.argv[2]
-    database = sys.argv[3]
+if __name__ == "__main__":
 
     db = MySQLdb.connect(
-        host='localhost',
+        host="localhost",
         port=3306,
-        user=username,
-        passwd=password,
-        db=database
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3]
     )
 
     cur = db.cursor()
-
     cur.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
 
-    states = cur.fetchall()
-    for state in states:
-        print(state)
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
 
     cur.close()
     db.close()
