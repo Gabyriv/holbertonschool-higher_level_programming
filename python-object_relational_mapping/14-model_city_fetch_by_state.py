9e49d2af135449abf9c239dd67e4834a7ed16449
Gabriel <gabyrivm06@gmail.com>
2024-10-29 19:12:02 -0400

    14-model_city_fetch_by_state.py

--- python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Script to list all City objects from the database"""
+"""Script that prints all City objects from the database hbtn_0e_14_usa"""
 
 import sys
 from sqlalchemy import create_engine
@@ -7,24 +7,21 @@ from sqlalchemy.orm import sessionmaker
 from model_state import Base, State
 from model_city import City
 
-
 if __name__ == "__main__":
-    if len(sys.argv) != 4:
-        print("Usage: {} username password database".format(sys.argv[0]))
-        sys.exit(1)
 
-    username, password, database = sys.argv[1:4]
-    connection_str = 'mysql+mysqldb://{}:{}@localhost:3306/{}'
-    engine = create_engine(connection_str.format(username, password, database),
-                           pool_pre_ping=True)
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
 
     Session = sessionmaker(bind=engine)
+
     session = Session()
 
-    query = session.query(State, City).filter(State.id == City.state_id)\
-                   .order_by(City.id)
+    cities = session.query(City, State)\
+        .join(State)\
+        .order_by(City.id)\
+        .all()
 
-    for state, city in query.all():
+    for city, state in cities:
         print("{}: ({}) {}".format(state.name, city.id, city.name))
 
     session.close()
