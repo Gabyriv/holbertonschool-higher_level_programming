d4a6ac39f6fd27600391b90d612ca9dd895729ec
Gabriel <gabyrivm06@gmail.com>
2024-10-29 09:40:55 -0400

    changed some keywords on the connect function

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -1,23 +1,25 @@
 #!/usr/bin/python3
 
 """
-Script that lists all states from a given database.
+Script that lists all states in the database.
 """
-
 
 import MySQLdb
 import sys
 
 if __name__ == "__main__":
+
     db = MySQLdb.connect(
-        host='localhost',
+        host="localhost",
         port=3306,
         user=sys.argv[1],
-        password=sys.argv[2],
-        db=sys.argv[3])
-    cur = db.cursor()
+        passwd=sys.argv[2],
+        db=sys.argv[3]
+        )
 
+    cur = db.cursor()
     cur.execute("SELECT * FROM states ORDER BY id ASC")
+
     rows = cur.fetchall()
     for row in rows:
         print(row)
