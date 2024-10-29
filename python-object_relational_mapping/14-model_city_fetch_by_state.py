06fa0c06869cc7ea1158d19e25af85958908ac7f
Gabriel <gabyrivm06@gmail.com>
2024-10-29 18:52:36 -0400

    second arguments

--- python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -13,7 +13,7 @@ from sqlalchemy.orm import sessionmaker
 
 if __name__ == '__main__':
     username = sys.argv[1]
-    password = ''
+    password = sys.argv[2]
     database = sys.argv[3]
 
     engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'
