fa305e28a7be1dd4006b9b6ac9f1533dd28a4329
Gabriel <gabyrivm06@gmail.com>
2024-10-30 17:49:57 -0400

    Adding relationships from city to states

--- python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -10,18 +10,16 @@ from model_city import City
 if __name__ == "__main__":
 
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
-                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+    Base.metadata.create_all(engine)
 
     Session = sessionmaker(bind=engine)
-
     session = Session()
 
-    cities = session.query(City, State)\
-        .join(State)\
-        .order_by(City.id)\
-        .all()
+    cities = session.query(City).order_by('City.id').all()
 
-    for city, state in cities:
-        print("{}: ({}) {}".format(state.name, city.id, city.name))
+    for city, in cities:
+        print("{}: ({}) {}".format(city.state.name, city.id, city.name))
 
     session.close()
--- python-object_relational_mapping/model_city.py
+++ python-object_relational_mapping/model_city.py
@@ -5,12 +5,17 @@ Model for city objects
 """
 
 from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy import relationship
 from model_state import Base
+
 
 class City(Base):
     """City class that inherits from Base"""
     __tablename__ = 'cities'
 
-    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
+    id = Column(Integer, primary_key=True, unique=True,
+                nullable=False, autoincrement=True)
     name = Column(String(128), nullable=False)
     state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
+
+    state = relationship('State')
