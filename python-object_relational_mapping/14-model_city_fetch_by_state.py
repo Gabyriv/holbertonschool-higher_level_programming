6e5f27fd8dd899573c080b17b6198d0d342e474e
Gabriel <gabyrivm06@gmail.com>
2024-10-30 17:57:03 -0400

    Added metadata

--- python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -8,18 +8,21 @@ from model_state import Base, State
 from model_city import City
 
 if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
 
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
-                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
-                           pool_pre_ping=True)
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        username, password, database), pool_pre_ping=True)
     Base.metadata.create_all(engine)
 
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    cities = session.query(City).order_by('City.id').all()
+    cities = session.query(City).order_by(City.id).all()
 
-    for city, in cities:
-        print("{}: ({}) {}".format(city.state.name, city.id, city.name))
+    for city in cities:
+        print("{}: ({}) {}".format(
+            city.state.name, city.id, city.name))
 
     session.close()
--- python-object_relational_mapping/model_city.py
+++ python-object_relational_mapping/model_city.py
@@ -1,21 +1,16 @@
 #!/usr/bin/python3
-
-"""
-Model for city objects
-"""
+"""Model for City objects"""
 
 from sqlalchemy import Column, Integer, String, ForeignKey
-from sqlalchemy import relationship
+from sqlalchemy.orm import relationship
 from model_state import Base
 
 
 class City(Base):
-    """City class that inherits from Base"""
+    """City class"""
     __tablename__ = 'cities'
-
     id = Column(Integer, primary_key=True, unique=True,
-                nullable=False, autoincrement=True)
+                autoincrement=True, nullable=False)
     name = Column(String(128), nullable=False)
     state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
-
     state = relationship('State')
