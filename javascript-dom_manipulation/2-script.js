77c2d11fea475484ab100437bcd3320a759fc443
Gabriel <gabyrivm06@gmail.com>
2024-11-22 11:31:10 -0400

    Semistandard error fixes

--- javascript-dom_manipulation/2-script.js
+++ javascript-dom_manipulation/2-script.js
@@ -1,5 +1,5 @@
-//Script that adds the class red to the header element when the user clicks on the tag with id red_header
+// Script that adds the class red to the header element when the user clicks on the tag with id red_header
 document.getElementById('red_header').addEventListener('click', function () {
-    let element = document.querySelector('header');
-    element.classList.add("red");
-})
+  const element = document.querySelector('header');
+  element.classList.add('red');
+});
